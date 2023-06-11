d077a3f132d43fe9b32d08c6e14d491ce5570880
Gabriel Kakizaki <gabriel.kakizaki@ufv.br>
2023-06-11 19:59:50 -0300

    Generate OCR for naive and only filtering pipelines

--- src/sibigrapi2023/pipelines/metric_generation/nodes.py
+++ src/sibigrapi2023/pipelines/metric_generation/nodes.py
@@ -4,11 +4,13 @@ generated using Kedro 0.18.7
 """
 
 import gc
+import time
 
 import logging
 
 log = logging.getLogger(__name__)
 
+import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 import cv2
@@ -19,6 +21,7 @@ import torchvision
 from ISR.models import RDN
 
 import pytesseract
+import imutils
 
 from skimage.restoration import estimate_sigma
 
@@ -28,15 +31,87 @@ def _clean_gpu_memory():
     gc.collect()
     torch.cuda.empty_cache()
 
-sam_checkpoint = "sam_vit_h_4b8939.pth"
-model_type = "vit_h"
-device = "cuda"
+#sam_checkpoint = "sam_vit_h_4b8949.pth"
+#model_type = "vit_h"
+#device = "cuda"
+#
+#sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+#sam.to(device=device)
+#
+#predictor = SamPredictor(sam)
 
-sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
-sam.to(device=device)
+def _biggest_contour(contours):
+        biggest = np.array([])
+        max_area = 0
+        for i in contours:
+            area = cv2.contourArea(i)
+            if area > 58000:
+                peri = cv2.arcLength(i, True)
+                approx = cv2.approxPolyDP(i, 0.015 * peri, True)
+                if area > max_area and len(approx) == 4:
+                    biggest = approx
+                    max_area = area
+        return biggest
 
-predictor = SamPredictor(sam)
+def _sharpnessTenengrad(img):
+    # Calculate the gradient of the image using the Sobel operator
+    sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=3)
+    sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=3)
+    grad = np.sqrt(sobelx**2 + sobely**2)
 
+    # Calculate the variance of the gradient image
+    return grad.var()
+
+
+def _estimateNoise(img):
+    return estimate_sigma(img, average_sigmas=True)
+
+def _order_points(pts):
+        rect = np.zeros((4, 2), dtype = "float32")
+        s = np.sum(pts,axis = 1)
+        rect[0] = pts[np.argmin(s)]
+        rect[2] = pts[np.argmax(s)]
+        diff = np.diff(pts, axis = 1)
+        rect[3] = pts[np.argmin(diff)]
+        rect[1] = pts[np.argmax(diff)]
+        # return the ordered coordinates
+        return rect
+
+def _getAngle(img):
+    image = imutils.resize(img, height = 500)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+    gray = cv2.bilateralFilter(gray, 20, 30, 30)
+    edged = cv2.Canny(gray, 50, 150)
+    
+  
+    cnts = None
+    cnts, hierarchy  = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
+
+    
+    screenCnt = None
+    screenCnt = _biggest_contour(cnts)
+    
+    if(len(screenCnt)==0 ):
+        edged = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,13,0.5)
+        cnts = None
+        cnts, hierarchy  = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
+        
+        screenCnt = None
+        screenCnt = _biggest_contour(cnts)
+
+    if(len(screenCnt) >0 ):
+        cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 1)
+        screenCnt = _order_points(screenCnt[:,0,:])
+        angle = cv2.minAreaRect(screenCnt)[-1]
+        angle = 90 - angle if (angle>45) else angle        
+        
+        return angle
+    else:
+        return None
 
 # pipeline de melhoramento
 def _binarizeAdaptive(image, T, C, show=False):
@@ -70,9 +145,9 @@ def _filterImages(imagesList):
 
   for img in imagesList:
 
-    imgBlurLevel = sharpnessTenengrad(img)
-    noiseLevel = estimateNoise(img)
-    imgAngle = getAngle(img)
+    imgBlurLevel = _sharpnessTenengrad(img)
+    noiseLevel = _estimateNoise(img)
+    imgAngle = _getAngle(img)
 
     #boas - aceitaveis
     if(imgBlurLevel >= 12111.474131839535 and noiseLevel<= 0.4516075407697839): #verifica ruido e nitidez
@@ -86,9 +161,9 @@ def _filterImages(imagesList):
   for img in imagesList:
     if(img in imagesGood): continue
 
-    imgBlurLevel = sharpnessTenengrad(img)
-    noiseLevel = estimateNoise(img)
-    imgAngle = getAngle(img)
+    imgBlurLevel = _sharpnessTenengrad(img)
+    noiseLevel = _estimateNoise(img)
+    imgAngle = _getAngle(img)
 
     #descartadas 
     if(imgAngle != None):
@@ -104,6 +179,19 @@ def _filterImages(imagesList):
             imagesMedium = np.append(imagesMedium,i)
 
   return (imagesGood, imagesMedium, imagesBad)
+
+def _fileterImagePipeline(img):
+    imgBlurLevel = _sharpnessTenengrad(img)
+    noiseLevel = _estimateNoise(img)
+    imgAngle = _getAngle(img)
+    if (imgBlurLevel >= 12111.474131839535 and noiseLevel<= 0.4516075407697839): #verifica ruido e nitidez
+        if(imgAngle != None): # verifica se conseguiu calcular um angulo
+            if(imgAngle <= 1.249346097310384):# se encaixa em todos os parametros classifica como bom
+                return pytesseract.image_to_string(img)
+        else:
+            return pytesseract.image_to_string(img)
+    else:
+        return ""
 
 def _unwarp(img, src, dst):
     h, w = img.shape[:2]
@@ -140,8 +228,26 @@ def _homography(image, points):
     warped_img, M = unwarp(image, src, dst)
     return warped_img, dst
 
-def generate_method_metrics(images):
-    pass
+def generate_methods_ocr_text(images):
+    all_results = dict()
+    pipeline_functions = {
+        "naive": lambda x: pytesseract.image_to_string(x),
+        "only_filtering": _fileterImagePipeline,
+    }
+    for pipeline_name, pipeline_func in pipeline_functions.items():
+        results = list()
+        for file_name, loader in images.items():
+            image = np.array(loader())
+            start = time.process_time()
+            ocr_text = pipeline_func(image)
+            pipeline_time = time.process_time() - start
+            results.append({
+                "file_name": file_name,
+                "pipeline_time": pipeline_time,
+                "ocr_text": ocr_text,
+            })
+        all_results[pipeline_name] = results
+    return all_results
 
 def generate_ablation_metrics(images):
     pass
--- src/sibigrapi2023/pipelines/metric_generation/pipeline.py
+++ src/sibigrapi2023/pipelines/metric_generation/pipeline.py
@@ -4,7 +4,7 @@ generated using Kedro 0.18.7
 """
 
 from .nodes import (
-    generate_method_metrics,
+    generate_methods_ocr_text,
     generate_ablation_metrics,
 )
 
@@ -12,4 +12,11 @@ from kedro.pipeline import Pipeline, node, pipeline
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline([
+        node(
+            func=generate_methods_ocr_text,
+            name="generate_methods_ocr_text_func",
+            inputs="express_small",
+            outputs="methods_ocr"
+        ),
+    ])
